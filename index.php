92456ef7b269b9fc10e51aefa81c5fc9aa51fddf
yfsanchez <yfsanchez@gmail.com>
2018-09-29 16:09:16 -0300

    segunda actualizacion

--- index.php
+++ index.php
@@ -1,5 +1,9 @@
 <?php
 echo "prueba";
 $variable = 123;
+$arreglo = $arrayName = array('1' => 123,'2' => 234 );
+foreach ($arreglo as $key => $value) {
+    echo $value;
+}
 var_dump($variable);
 ?>
